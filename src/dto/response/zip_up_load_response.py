99f5485fa8ed6fbfe1677528c589be7976fc4e00
trong <cutocm989@gmail.com>
2026-03-25 07:31:10 +0700

    chuẩn hoá lại cho đúng sách giáo khoa

--- src/dto/response/zip_up_load_response.py
+++
@@ -1,6 +0,0 @@
-class UploadZipResponse:
-    def __init__(self, errorMessage: list[str], total: int, failedTotal: int, totalSuccess:int):
-        self.errorMessage = errorMessage
-        self.total = total
-        self.failedTotal = failedTotal
-        self.totalSuccess = totalSuccess
